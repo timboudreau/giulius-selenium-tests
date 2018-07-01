89997fc731fdfa8cdb6ddb1d7ec286df8148885e
Tim Boudreau <tim@timboudreau.com>
2018-07-01 16:00:25 -0400

    Factor out annotation processors; generate meta-inf file for @Inject; acteur-resources improvements

--- test-main/src/main/java/com/mastfrog/testmain/suites/SuitesProcessor.java
+++ test-main/src/main/java/com/mastfrog/testmain/suites/SuitesProcessor.java
@@ -1,12 +1,12 @@
 package com.mastfrog.testmain.suites;
 
-import com.mastfrog.util.service.IndexGeneratingProcessor;
+import com.mastfrog.util.service.AbstractSingleAnnotationLineOrientedRegistrationAnnotationProcessor;
 import com.mastfrog.util.service.ServiceProvider;
+import java.lang.annotation.Annotation;
 import java.util.Collections;
 import java.util.Set;
 import java.util.regex.Pattern;
 import javax.annotation.processing.Processor;
-import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
@@ -21,33 +21,41 @@ import javax.tools.Diagnostic;
 @ServiceProvider(Processor.class)
 @SupportedAnnotationTypes({"com.mastfrog.testmain.suites.Suites"})
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
-public class SuitesProcessor extends IndexGeneratingProcessor {
-	private static final Pattern whitespace = Pattern.compile("\\s");
+public class SuitesProcessor extends AbstractSingleAnnotationLineOrientedRegistrationAnnotationProcessor {
 
-	@Override
-	public Set<String> getSupportedAnnotationTypes() {
-		return Collections.singleton(Suites.class.getName());
-	}
+    private static final Pattern whitespace = Pattern.compile("\\s");
+    private int ix;
 
-	@Override
-	protected boolean handleProcess(Set<? extends TypeElement> annotations, RoundEnvironment round) {
-		for (Element el : round.getElementsAnnotatedWith(Suites.class)) {
-			TypeElement te = (TypeElement) el;
-			Suites suites = te.getAnnotation(Suites.class);
-			for (String suiteName : suites.value()) {
-				if (suiteName.contains(":")) {
-					processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Suite name may not contain a colon: '" + suiteName + "'", te);
-					continue;
-				}
-				if (whitespace.matcher(suiteName).matches()) {
-					processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Suite name may not contain whitespace: '" + suiteName + "'", te);
-					continue;
-				}
-				
-				String line = suiteName + ":" + te.getQualifiedName();
-				super.addLine(Suites.SUITES_FILE, line, el);
-			}
-		}
-		return false;
-	}
+    SuitesProcessor() {
+        super(Suites.class);
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Collections.singleton(Suites.class.getName());
+    }
+
+    @Override
+    protected void doHandleOne(Element el, Annotation anno, int order) {
+        TypeElement te = (TypeElement) el;
+        Suites suites = te.getAnnotation(Suites.class);
+        for (String suiteName : suites.value()) {
+            if (suiteName.contains(":")) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Suite name may not contain a colon: '" + suiteName + "'", te);
+                continue;
+            }
+            if (whitespace.matcher(suiteName).matches()) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Suite name may not contain whitespace: '" + suiteName + "'", te);
+                continue;
+            }
+
+            String line = suiteName + ":" + te.getQualifiedName();
+            super.addLine(Suites.SUITES_FILE, line, el);
+        }
+    }
+
+    @Override
+    protected int order(Annotation anno) {
+        return ix++;
+    }
 }
